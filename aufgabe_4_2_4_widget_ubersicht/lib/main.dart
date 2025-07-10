859df698b0e918267c3238049079ce4eaa2da667
Linda Sixtus <linda.sixtus@gmx.de>
2025-07-10 10:56:36 +0200

    Korrektur

--- aufgabe_4_2_4_widget_ubersicht/lib/main.dart
+++ aufgabe_4_2_4_widget_ubersicht/lib/main.dart
@@ -12,76 +12,103 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Aufgabe 1'), backgroundColor: Colors.blue),
+
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Hello App Akademie!" ,
-              textAlign: TextAlign.left, 
+            Padding(padding: const EdgeInsets.all(10.0)),
+            Padding(padding: const EdgeInsets.only(left: 10.0),
+              child: Text(
+              "Hello App Akademie!",
+              textAlign: TextAlign.left,
               style: TextStyle(
-                fontWeight: FontWeight.bold, 
-                fontSize: 24, 
-                color: Colors.blue)),
-            Row(children: [
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+                color: Colors.blue,
+              ),
+            )),
+        
+            SizedBox(height: 20), // Abstand zwischen Text und Container
+            Row(
+              children: [
+                SizedBox(width: 10), // Abstand am Anfang der Zeile
                 Container(
                   alignment: Alignment.center,
-                  color: Colors.red, width: 100,
-                  height: 100, 
-                  child: 
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      color: Color.fromARGB(255, 162, 30, 233), 
-                      width: 50, 
-                      height: 50, 
-                      child: 
-                        Text("A", 
-                          style: TextStyle(fontSize: 24, color: Colors.white), 
-                          textAlign: TextAlign.center, 
-                        ),
-                    )
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100.0),
+                      color: Color.fromARGB(255, 162, 30, 233),
+                    ),
+
+                    width: 50,
+                    height: 35,
+                    alignment: Alignment.center,
+                    child: Text(
+                      "A",
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ),
+                SizedBox(width: 10), // Abstand zwischen den Containern
                 Container(
                   alignment: Alignment.center,
-                  color: Colors.green, width: 100,
-                  height: 100, 
-                  child: 
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      color: Color.fromARGB(255, 162, 30, 233), 
-                      width: 50, 
-                      height: 50, 
-                      child: 
-                        Text("A", 
-                          style: TextStyle(fontSize: 24, color: Colors.white), 
-                          textAlign: TextAlign.center, 
-                        ),
-                    )
+                  color: Colors.green,
+                  width: 100,
+                  height: 100,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100.0),
+                      color: Color.fromARGB(255, 162, 30, 233),
+                    ),
+                    width: 50,
+                    height: 35,
+                    alignment: Alignment.center,
+                    child: Text(
+                      "B",
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ),
+                SizedBox(width: 10),
                 Container(
                   alignment: Alignment.center,
-                  color: Colors.blue, width: 100,
-                  height: 100, 
-                  child: 
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      color: Color.fromARGB(255, 162, 30, 233), 
-                      width: 50, 
-                      height: 50, 
-                      child: 
-                        Text("A", 
-                          style: TextStyle(fontSize: 24, color: Colors.white), 
-                          textAlign: TextAlign.center, 
-                        ),
-                    )
+                  color: Colors.blue,
+                  width: 100,
+                  height: 100,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100.0),
+                      color: Color.fromARGB(255, 162, 30, 233),
+                    ),
+                    width: 50,
+                    height: 35,
+                    alignment: Alignment.center,
+                    child: Text(
+                      "C",
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ),
-          ])
-            ],)
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.face, size: 30, color: Colors.black),
+                Icon(Icons.face, size: 30, color: Colors.black),
+              ],
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
